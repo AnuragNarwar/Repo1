a4dcb1fed382965eaef34c52a29ca700455ac22c
AnuragNarwar <72944613+AnuragNarwar@users.noreply.github.com>
2021-07-13 12:11:01 +0530

    Update Pangram.java

--- Programs/src/Pangram.java
+++ Programs/src/Pangram.java
@@ -1,7 +1,18 @@
 public class Pangram {
     
   public static void main(String[] args){
-    System.out.println(0);
+    String str = "The quick brown Fox jumped over";
+      char[] missingChar = findMissingChar(str);
   }
+    public static char[] findMissingChar(String str){
+        ArrayList<Character> missingChar = new ArrayList<>();
+        boolean[] character = new boolean[26];
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)>= 'A' && str.charAt(i)>= 'Z')
+                character[str.charAt(i) - 'A'] = true;
+            else if(str.charAt(i)>= 'a' && str.charAt(i)>= 'z')
+                character[str.charAt(i) - 'a'] = true;
+        }
+    }
    
 }
