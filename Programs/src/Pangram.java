3c5f74d7dac02bead70bf763b2ff51bd06ae20c6
AnuragNarwar <72944613+AnuragNarwar@users.noreply.github.com>
2021-07-13 14:56:38 +0530

    Update Pangram.java
    complete program to find missing characters to make a string a Pangram

--- Programs/src/Pangram.java
+++ Programs/src/Pangram.java
@@ -1,18 +1,25 @@
 public class Pangram {
     
   public static void main(String[] args){
-    String str = "The quick brown Fox jumped over";
-      char[] missingChar = findMissingChar(str);
+    String str = "The quick brown Fox jumps";
+      ArrayList<Character> missingChar = findMissingChar(str);
+      System.out.println(missingChar);
   }
-    public static char[] findMissingChar(String str){
+    public static ArrayList<Character> findMissingChar(String str){
         ArrayList<Character> missingChar = new ArrayList<>();
         boolean[] character = new boolean[26];
         for(int i=0; i<str.length(); i++){
-            if(str.charAt(i)>= 'A' && str.charAt(i)>= 'Z')
+            if(str.charAt(i)>= 'A' && str.charAt(i)<= 'Z')
                 character[str.charAt(i) - 'A'] = true;
-            else if(str.charAt(i)>= 'a' && str.charAt(i)>= 'z')
+            else if(str.charAt(i)>= 'a' && str.charAt(i)<= 'z')
                 character[str.charAt(i) - 'a'] = true;
         }
+        for(int i=0; i<26; i++){
+            if(character[i]==false){
+                missingChar.add((char)('a'+i));
+            }
+        }
+        return missingChar;
     }
    
 }
