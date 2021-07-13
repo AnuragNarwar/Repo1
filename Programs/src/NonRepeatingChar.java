43c496c1539fda1ffaa46820c4a3083ea551feee
AnuragNarwar <72944613+AnuragNarwar@users.noreply.github.com>
2021-07-13 11:08:00 +0530

    Update NonRepeatingChar.java
    Complete code without collection

--- Programs/src/NonRepeatingChar.java
+++ Programs/src/NonRepeatingChar.java
@@ -1,7 +1,23 @@
 public class NonRepeatingChar {
 
   public static void main(String[] args){
-    System.out.println("Testing");
+      String string = "aabbcddeefgg";
+      char ch = nonRepeating(string);
+    System.out.println(ch);
   }
   
+  public static char nonRepeating(String str){
+      int index=-1;
+      char count[] = new char[256];
+      for(int i=0; i<str.length(); i++){
+          count[str.charAt(i)]++;
+      }
+      for(int i=0; i<str.length(); i++){
+          if(count[str.charAt(i)]==1){
+              return str.charAt(i);
+          }
+      }
+      
+      return '0';
+  }
 }
